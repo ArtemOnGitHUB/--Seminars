4f7af73f7b25a0adcd3624a5bf5efe755e318f84
Artem <kuzmichev8-1991@list.ru>
2022-12-12 16:26:26 +0300

    1.2

---
+++ 1_lesson/1.2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число недели ");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num1 == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num1 == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num1 == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num1 == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num1 == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (num1 == 2)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("error");
+}
