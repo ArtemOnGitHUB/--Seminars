632e3ff7643b3620829e9d7a9db474f38f4cf85c
Artem <kuzmichev8-1991@list.ru>
2022-12-12 17:09:45 +0300

    Fourth DZ

---
+++ HomeWork/Lesson1/1.4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int startNumber = 1;
+
+
+while(startNumber <= number)
+    {
+        if(startNumber % 2 ==0)
+        Console.WriteLine(startNumber + ", ");
+        startNumber ++;
+    }
