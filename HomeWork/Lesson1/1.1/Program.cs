d8eb1f643f7198486183090d387062a1609e3587
Artem <kuzmichev8-1991@list.ru>
2022-12-12 16:54:26 +0300

    first DZ

---
+++ HomeWork/Lesson1/1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber + " > " + secondNumber);
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine(secondNumber + " > " + firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber + " = " + firstNumber);
+}
