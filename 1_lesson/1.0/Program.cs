decebf35d84d99f9efbea5793e57e0b31f41913c
Artem <kuzmichev8-1991@list.ru>
2022-12-12 16:14:16 +0300

    1 zadacha

--- 1_lesson/1.0/Program.cs
+++ 1_lesson/1.0/Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine("Write a number : ");
-
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine(num * num);
+﻿
---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write a number 1 : ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number 2 : ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == (num1*num1))
+{
+    Console.WriteLine("ДА");
+}
+else 
+    Console.WriteLine("NO");
