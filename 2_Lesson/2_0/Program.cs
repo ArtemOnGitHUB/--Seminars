e7efcbc0da5a1db259ef3cecdd2551c9fa425420
Artem <kuzmichev8-1991@list.ru>
2022-12-15 12:19:17 +0300

    new task 2_0

---
+++ 2_Lesson/2_0/Program.cs
@@ -0,0 +1,19 @@
+﻿int MaxNumber(int num)
+{
+    Console.WriteLine(num);
+
+    if(num % 10 > num / 10)
+        return num % 10;
+    else 
+        return num / 10;
+}
+
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
+
+// 123456 % 10 - 6
+// 123456 / 10 - 12345
+
+//12 % 10 - 2
+//12 / 10 - 1
+
+//12345 / 100 - 1234
